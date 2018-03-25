06c4dd3dbc8ac95c77a8602c08549d3f263d61b5
derek chen <derek.g.chen@gmail.com>
2018-03-25 21:10:28 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -56,5 +56,9 @@ def get_average_age_for_season(data, season)
   data[season].each do |result|
       age << result["age"]
   end
-  age
+  avg_age = 0
+  age.each do |v|
+    avg_age += parseInt(v)
+  
+  avg_age/age.size
 end
