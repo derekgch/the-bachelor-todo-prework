2e527d54b6d903b64a4e2abc53f026fdd040626a
derek chen <derek.g.chen@gmail.com>
2018-03-25 21:05:35 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -38,7 +38,7 @@ end
 
 def get_occupation(data, hometown)
   # code here
-  
+
   data.each do |season, contestants|
     contestants.each do |result|
       if result["hometown"] == hometown
@@ -47,7 +47,7 @@ def get_occupation(data, hometown)
     end
   end
 
-  
+
 end
 
 def get_average_age_for_season(data, season)
